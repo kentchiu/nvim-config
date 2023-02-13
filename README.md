4141c6a65146a3de6e98d1c7d837e54922b633b2
Kent Chiu <kent.cwg@gmail.com>
2023-02-13 21:24:18 +0800

    remove test plugin and options

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# 💤 LazyVim
+# Nvim 
 
-A starter template for [LazyVim](https://github.com/LazyVim/LazyVim).
-Refer to the [documentation](https://lazyvim.github.io/installation) to get started.
+Powered by lazyvim
+
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,9 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.set(
-  "n",
-  "<leader>sx",
-  require("telescope.builtin").resume,
-  { noremap = true, silent = true, desc = "Resume" }
-)
+
+vim.keymap.set("i", "jj", "<Esc>", { desc = "Escape" })
+
+-- To use CTRL+{h,j,k,l} to navigate windows from terminal in any mode
+vim.keymap.set("t", "<C-h>", "<C-\\><C-n><C-w>h", { desc = "Go To Left Window In Terminal" })
+vim.keymap.set("t", "<C-j>", "<C-\\><C-n><C-w>j", { desc = "Go To Bottom Window In Terminal" })
+vim.keymap.set("t", "<C-k>", "<C-\\><C-n><C-w>k", { desc = "Go Top Up Window In Terminal" })
+vim.keymap.set("t", "<C-l>", "<C-\\><C-n><C-w>l", { desc = "Go Right Up Window In Terminal" })
