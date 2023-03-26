008e2625d146ebd77c9341c2efbebfe439c44759
Kent Chiu <kent.cwg@gmail.com>
2023-03-27 01:18:15 +0800

    file status

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -175,15 +175,8 @@ return {
       { "<s-tab>", function() require("luasnip").jump(-1) end, mode = { "i", "s" } },
     },
   },
-  { "Pocco81/auto-save.nvim", enabled = false },
+  { "Pocco81/auto-save.nvim",               enabled = false },
   { "nvim-telescope/telescope-symbols.nvim" },
-  -- { -- move to themes
-  --   "navarasu/onedark.nvim",
-  --   -- priority = 1000,
-  --   -- config = function()
-  --   --   vim.cmd.colorscheme "onedark"
-  --   -- end,
-  -- },
   {
     -- Set lualine as statusline
     "nvim-lualine/lualine.nvim",
@@ -194,6 +187,34 @@ return {
         theme = "auto",
         component_separators = "|",
         section_separators = "",
+      },
+      sections = {
+        lualine_c = {
+          {
+            "filetype",
+            icon_only = true,
+            separator = "",
+            padding = { right = 0, left = 1 },
+          },
+          {
+            'filename',
+            file_status = true, -- Displays file status (readonly status, modified status)
+            newfile_status = true, -- Display new file status (new file means no write after created)
+            path = 3,         -- 0: Just the filename
+            -- 1: Relative path
+            -- 2: Absolute path
+            -- 3: Absolute path, with tilde as the home directory
+
+            shorting_target = 40, -- Shortens path to leave 40 spaces in the window
+            -- for other components. (terrible name, any suggestions?)
+            symbols = {
+              modified = ' ❗❗❗',     -- Text to show when the buffer is modified
+              readonly = ' 🔒',      -- Text to show when the file is non-modifiable or readonly.
+              unnamed = '[No Name]', -- Text to show for unnamed buffers.
+              newfile = '[New]',
+            },
+          }
+        },
       },
     },
   },
