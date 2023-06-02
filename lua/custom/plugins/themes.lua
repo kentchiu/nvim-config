012ab807130a6f94da750ed1e5f4d24d68596f0c
Kent Chiu <kent.cwg@gmail.com>
2023-06-02 17:59:02 +0800

    catppuccin theme

--- lua/custom/plugins/themes.lua
+++ lua/custom/plugins/themes.lua
@@ -1,50 +1,25 @@
 return {
-  -- {
-  --   "navarasu/onedark.nvim",
-  --   -- priority = 1000,
-  --   -- config = function()
-  --   --   vim.cmd.colorscheme "onedark"
-  --   -- end,
-  -- },
-  {
-    "rebelot/kanagawa.nvim",
-    priority = 1000,
-    -- config = function()
-    --   vim.cmd.colorscheme "kanagawa"
-    -- end,
-  },
-  {
-    "olimorris/onedarkpro.nvim",
-    priority = 1000,
-    -- config = function()
-    --   vim.cmd.colorscheme "onedark"
-    -- end,
-  },
-  {
-    "folke/tokyonight.nvim",
-    priority = 1000,
-    -- config = function()
-    --   vim.cmd.colorscheme "tokyonight"
-    -- end,
-  },
-  {
-    "Mofiqul/dracula.nvim",
-    priority = 1000,
-    -- config = function()
-    --   vim.cmd.colorscheme "dracula"
-    -- end,
-  },
+  { "rebelot/kanagawa.nvim", },
+  { "olimorris/onedarkpro.nvim", },
+  { "folke/tokyonight.nvim", },
+  { "Mofiqul/dracula.nvim", },
+  { "catppuccin/nvim" },
   {
     'projekt0n/github-nvim-theme',
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       require('github-theme').setup({
       })
-
       -- random theme
-      local items = { "onedark", "kanagawa", "tokyonight", "dracula",
-        "github_light", "github_light_default",
-        "github_dark", "github_dark_dimmed", "github_dark_high_contrast" }
+      local items = {
+        "onedark", "onelight", "onedark_vivid", "onedark_dark",
+        "tokyonight", "tokyonight-night", "tokyonight-storm", "tokyonight-day", "tokyonight-moon", 
+        "dracula", "dracula-soft",
+        "kanagawa", "kanagawa-wave", "kanagawa-dragon", "kanagawa-lotus",
+        -- "github_light", "github_light_high_contrast",
+        "github_dark", "github_dark_dimmed", "github_dark_high_contrast" ,
+        "catppuccin", "catppuccin-latte", "catppuccin-frappe", "catppuccin-macchiato", "catppuccin-mocha",
+      }
 
       math.randomseed(os.time())
       local random_index = math.random(1, #items)
