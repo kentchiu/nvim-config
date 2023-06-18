dc1ff7baf45a2bede4ea0ea457f66760dd87f8e7
Kent Chiu <kent.cwg@gmail.com>
2023-06-19 00:48:49 +0800

    dap debug

--- lua/custom/plugins/neo-test.lua
+++ lua/custom/plugins/neo-test.lua
@@ -8,36 +8,38 @@ return {
   },
   keys = {
     { "<leader>;",  function() require("neotest").run.run() end,                     desc = "Test Current Method" },
-    { "<leader>ts", function() require("neotest").summary.toggle() end,              desc = "Toggle Test Summary" },
     { "<leader>tm", function() require("neotest").run.run() end,                     desc = "Test Current Method" },
     { "<leader>tf", function() require("neotest").run.run(vim.fn.expand("%")) end,   desc = "Test Current File" },
     { "<leader>ta", function() require("neotest").run.run(vim.fn.getcwd()) end,      desc = "Test All" },
-    { "<leader>tl", function() require("neotest").run.run({ strategy = "dap" }) end, desc = "Debug The Nearest Test" },
+    { "<leader>td", function() require("neotest").run.run({ strategy = "dap" }) end, desc = "Debug The Nearest Test" },
+    { "<leader>ts", function() require("neotest").summary.toggle() end,              desc = "Toggle Test Summary" },
     { "<leader>to", function() require("neotest").output.open({ enter = true }) end, desc = "Output Of Test Results" },
     { "<leader>tp", function() require("neotest").output_panel.toggle() end,         desc = "Output Terminal" },
   },
   config = function()
     require("neotest").setup({
-      quickfix = {
-        enabled = false,
-        open = false
-      },
+      -- quickfix = {
+      --   enabled = false,
+      --   open = false
+      -- },
       adapters = {
-
         require("neotest-python")({
           -- Extra arguments for nvim-dap configuration
           -- See https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for values
-          dap = { 
+          dap = {
             justMyCode = false,
             -- console = "integratedTerminal",
+            -- stopOnEntry = false, -- which is the default(false)
+            -- subProcess = false, -- see config/testing.lua
+            -- openUIOnEntry = false,
           },
           -- Command line arguments for runner
           -- Can also be a function to return dynamic values
-          args = { "--log-level", "DEBUG", "--quiet" },
+          -- args = { "--log-level", "DEBUG" },
           -- args = { "-vv", "-s" },
           -- Runner to use. Will use pytest if available by default.
           -- Can be a function to return dynamic value.
-          runner = "pytest",
+          -- runner = "pytest",
           -- Custom python path for the runner.
           -- Can be a string or a list of strings.
           -- Can also be a function to return dynamic value.
