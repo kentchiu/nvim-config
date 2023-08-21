4cc8847369f1f02fffc830232eb8b4e51d7957f0
Kent Chiu <kent.cwg@gmail.com>
2023-08-21 17:55:26 +0800

    chatgpt

---
+++ lua/custom/plugins/chatgpt.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "jackMort/ChatGPT.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim"
+    },
+    config = function()
+      require("chatgpt").setup({
+        openai_params = {
+          model = "gpt-4",
+        },
+        actions_paths = {
+        },
+      })
+    end,
+  }
+}
+
