7c1f2bec8349aa0784a239db29917f4bf76f0c08
Kent Chiu <kent.cwg@gmail.com>
2023-10-12 00:59:31 +0800

    fix debug icon

--- lua/custom/plugins/debug.lua
+++ lua/custom/plugins/debug.lua
@@ -1,12 +1,12 @@
 return {
-  {
-    "mfussenegger/nvim-dap",
+	{
+		"mfussenegger/nvim-dap",
 
-    dependencies = {
+		dependencies = {
 
-      -- fancy UI for the debugger
-      {
-        "rcarriga/nvim-dap-ui",
+			-- fancy UI for the debugger
+			{
+				"rcarriga/nvim-dap-ui",
         -- stylua: ignore
         keys = {
           { "<leader>du", function() require("dapui").toggle({}) end,             desc = "Dap UI" },
@@ -26,145 +26,144 @@ return {
               "n", "v" }
           },
         },
-        opts = {},
-        config = function(_, opts)
-          local dap = require("dap")
-          local dapui = require("dapui")
-          dapui.setup(opts)
-          dap.listeners.after.event_initialized["dapui_config"] = function()
-            dapui.open({})
-          end
-          dap.listeners.before.event_terminated["dapui_config"] = function()
-            dapui.close({})
-          end
-          dap.listeners.before.event_exited["dapui_config"] = function()
-            dapui.close({})
-          end
+				opts = {},
+				config = function(_, opts)
+					local dap = require("dap")
+					local dapui = require("dapui")
+					dapui.setup(opts)
+					dap.listeners.after.event_initialized["dapui_config"] = function()
+						dapui.open({})
+					end
+					dap.listeners.before.event_terminated["dapui_config"] = function()
+						dapui.close({})
+					end
+					dap.listeners.before.event_exited["dapui_config"] = function()
+						dapui.close({})
+					end
 
-          -- highlighting
-          local dap_breakpoint_color = {
-            breakpoint = {
-              ctermbg = 0,
-              fg = '#993939',
-              bg = '#31353f',
-            },
-            logpoing = {
-              ctermbg = 0,
-              fg = '#61afef',
-              bg = '#31353f',
-            },
-            stopped = {
-              ctermbg = 0,
-              fg = '#98c379',
-              bg = '#31353f'
-            },
-          }
+					-- highlighting
+					local dap_breakpoint_color = {
+						breakpoint = {
+							ctermbg = 0,
+							fg = "#993939",
+							bg = "#31353f",
+						},
+						logpoing = {
+							ctermbg = 0,
+							fg = "#61afef",
+							bg = "#31353f",
+						},
+						stopped = {
+							ctermbg = 0,
+							fg = "#98c379",
+							bg = "#31353f",
+						},
+					}
 
-          vim.api.nvim_set_hl(0, 'DapBreakpoint', dap_breakpoint_color.breakpoint)
-          vim.api.nvim_set_hl(0, 'DapLogPoint', dap_breakpoint_color.logpoing)
-          vim.api.nvim_set_hl(0, 'DapStopped', dap_breakpoint_color.stopped)
+					vim.api.nvim_set_hl(0, "DapBreakpoint", dap_breakpoint_color.breakpoint)
+					vim.api.nvim_set_hl(0, "DapLogPoint", dap_breakpoint_color.logpoing)
+					vim.api.nvim_set_hl(0, "DapStopped", dap_breakpoint_color.stopped)
 
-          local dap_breakpoint = {
-            error = {
-              text = "",
-              texthl = "DapBreakpoint",
-              linehl = "DapBreakpoint",
-              numhl = "DapBreakpoint",
-            },
-            condition = {
-              text = 'ﳁ',
-              texthl = 'DapBreakpoint',
-              linehl = 'DapBreakpoint',
-              numhl = 'DapBreakpoint',
-            },
-            rejected = {
-              text = "",
-              texthl = "DapBreakpint",
-              linehl = "DapBreakpoint",
-              numhl = "DapBreakpoint",
-            },
-            logpoint = {
-              text = '',
-              texthl = 'DapLogPoint',
-              linehl = 'DapLogPoint',
-              numhl = 'DapLogPoint',
-            },
-            stopped = {
-              text = '',
-              texthl = 'DapStopped',
-              linehl = 'DapStopped',
-              numhl = 'DapStopped',
-            },
-          }
+					local dap_breakpoint = {
+						error = {
+							text = "",
+							texthl = "DapBreakpoint",
+							linehl = "DapBreakpoint",
+							numhl = "DapBreakpoint",
+						},
+						condition = {
+							text = "",
+							texthl = "DapBreakpoint",
+							linehl = "DapBreakpoint",
+							numhl = "DapBreakpoint",
+						},
+						rejected = {
+							text = "",
+							texthl = "DapBreakpint",
+							linehl = "DapBreakpoint",
+							numhl = "DapBreakpoint",
+						},
+						logpoint = {
+							text = "",
+							texthl = "DapLogPoint",
+							linehl = "DapLogPoint",
+							numhl = "DapLogPoint",
+						},
+						stopped = {
+							text = "",
+							texthl = "DapStopped",
+							linehl = "DapStopped",
+							numhl = "DapStopped",
+						},
+					}
 
-          vim.fn.sign_define('DapBreakpoint', dap_breakpoint.error)
-          vim.fn.sign_define('DapBreakpointCondition', dap_breakpoint.condition)
-          vim.fn.sign_define('DapBreakpointRejected', dap_breakpoint.rejected)
-          vim.fn.sign_define('DapLogPoint', dap_breakpoint.logpoint)
-          vim.fn.sign_define('DapStopped', dap_breakpoint.stopped)
+					vim.fn.sign_define("DapBreakpoint", dap_breakpoint.error)
+					vim.fn.sign_define("DapBreakpointCondition", dap_breakpoint.condition)
+					vim.fn.sign_define("DapBreakpointRejected", dap_breakpoint.rejected)
+					vim.fn.sign_define("DapLogPoint", dap_breakpoint.logpoint)
+					vim.fn.sign_define("DapStopped", dap_breakpoint.stopped)
 
+					-- vim.fn.sign_define('DapBreakpoint', { text = '●', texthl = 'DapBreakpoint' })
+				end,
+			},
 
-          -- vim.fn.sign_define('DapBreakpoint', { text = '●', texthl = 'DapBreakpoint' })
-        end,
-      },
+			-- virtual text for the debugger
+			{
+				"theHamsta/nvim-dap-virtual-text",
+				opts = {
+					enabled = true,
+					enable_commands = true,
+					highlight_changed_variables = true,
+					highlight_new_as_changed = false,
+					show_stop_reason = true,
+					commented = false,
+					only_first_definition = true,
+					all_references = false,
+					filter_references_pattern = "<module",
+					virt_text_pos = "eol",
+					all_frames = false,
+					virt_lines = false,
+					virt_text_win_col = nil,
+				},
+			},
 
-      -- virtual text for the debugger
-      {
-        "theHamsta/nvim-dap-virtual-text",
-        opts = {
-          enabled = true,
-          enable_commands = true,
-          highlight_changed_variables = true,
-          highlight_new_as_changed = false,
-          show_stop_reason = true,
-          commented = false,
-          only_first_definition = true,
-          all_references = false,
-          filter_references_pattern = '<module',
-          virt_text_pos = 'eol',
-          all_frames = false,
-          virt_lines = false,
-          virt_text_win_col = nil
-        },
-      },
+			-- which key integration
+			{
+				"folke/which-key.nvim",
+				optional = true,
+				opts = {
+					defaults = {
+						["<leader>d"] = { name = "+debug" },
+						["<leader>da"] = { name = "+adapters" },
+					},
+				},
+			},
 
-      -- which key integration
-      {
-        "folke/which-key.nvim",
-        optional = true,
-        opts = {
-          defaults = {
-            ["<leader>d"] = { name = "+debug" },
-            ["<leader>da"] = { name = "+adapters" },
-          },
-        },
-      },
+			-- mason.nvim integration
+			{
+				"jay-babu/mason-nvim-dap.nvim",
+				dependencies = "mason.nvim",
+				cmd = { "DapInstall", "DapUninstall" },
+				opts = {
+					-- Makes a best effort to setup the various debuggers with
+					-- reasonable debug configurations
+					automatic_installation = true,
 
-      -- mason.nvim integration
-      {
-        "jay-babu/mason-nvim-dap.nvim",
-        dependencies = "mason.nvim",
-        cmd = { "DapInstall", "DapUninstall" },
-        opts = {
-          -- Makes a best effort to setup the various debuggers with
-          -- reasonable debug configurations
-          automatic_installation = true,
+					-- You can provide additional configuration to the handlers,
+					-- see mason-nvim-dap README for more information
+					handlers = {},
 
-          -- You can provide additional configuration to the handlers,
-          -- see mason-nvim-dap README for more information
-          handlers = {},
+					-- You'll need to check that you have the required things installed
+					-- online, please don't ask me how to install them :)
+					ensure_installed = {
+						-- Update this to ensure that you have the debuggers for the langs you want
+					},
+				},
+			},
 
-          -- You'll need to check that you have the required things installed
-          -- online, please don't ask me how to install them :)
-          ensure_installed = {
-            -- Update this to ensure that you have the debuggers for the langs you want
-          },
-        },
-      },
-
-      -- Add your own debuggers here
-      'mfussenegger/nvim-dap-python',
-    },
+			-- Add your own debuggers here
+			"mfussenegger/nvim-dap-python",
+		},
 
     -- stylua: ignore
     keys = {
@@ -279,27 +278,27 @@ return {
         desc = "Widgets"
       },
     },
-    -- maps.n["<F29>"] = { function() require("dap").restart_frame() end, desc = "Debugger: Restart" } -- Control+F5
-    -- maps.n["<leader>dB"] = { function() require("dap").clear_breakpoints() end, desc = "Clear Breakpoints" }
-    -- vim.keymap.set('n', '<Leader>lp', function() require('dap').set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end)
-    -- vim.keymap.set('n', '<Leader>dr', function() require('dap').repl.open() end)
-    -- vim.keymap.set({'n', 'v'}, '<Leader>dp', function() require('dap.ui.widgets').preview() end)
-    -- vim.keymap.set('n', '<Leader>df', function() local widgets = require('dap.ui.widgets') widgets.centered_float(widgets.frames) end)
-    -- vim.keymap.set('n', '<Leader>ds', function() local widgets = require('dap.ui.widgets') widgets.centered_float(widgets.scopes) end)
-    -- Close debugger and clear breakpoints
-    -- vim.keymap.set("n", "<localleader>de", function()
-    --   dap.clear_breakpoints()
-    --   ui.toggle({})
-    --   dap.terminate()
-    --   vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-w>=", false, true, true), "n", false)
-    --   require("notify")("Debugger session ended", "warn")
-    -- end)
-    -- b = { "<cmd>lua require'dap'.step_back()<cr>", "Step Back" },
+		-- maps.n["<F29>"] = { function() require("dap").restart_frame() end, desc = "Debugger: Restart" } -- Control+F5
+		-- maps.n["<leader>dB"] = { function() require("dap").clear_breakpoints() end, desc = "Clear Breakpoints" }
+		-- vim.keymap.set('n', '<Leader>lp', function() require('dap').set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end)
+		-- vim.keymap.set('n', '<Leader>dr', function() require('dap').repl.open() end)
+		-- vim.keymap.set({'n', 'v'}, '<Leader>dp', function() require('dap.ui.widgets').preview() end)
+		-- vim.keymap.set('n', '<Leader>df', function() local widgets = require('dap.ui.widgets') widgets.centered_float(widgets.frames) end)
+		-- vim.keymap.set('n', '<Leader>ds', function() local widgets = require('dap.ui.widgets') widgets.centered_float(widgets.scopes) end)
+		-- Close debugger and clear breakpoints
+		-- vim.keymap.set("n", "<localleader>de", function()
+		--   dap.clear_breakpoints()
+		--   ui.toggle({})
+		--   dap.terminate()
+		--   vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-w>=", false, true, true), "n", false)
+		--   require("notify")("Debugger session ended", "warn")
+		-- end)
+		-- b = { "<cmd>lua require'dap'.step_back()<cr>", "Step Back" },
 
-    config = function()
-      -- Install golang specific config
-      require('dap-python').setup('./.venv/bin/python')
-      require('dap-python').test_runner = 'pytest'
-    end,
-  }
+		config = function()
+			-- Install golang specific config
+			require("dap-python").setup("./.venv/bin/python")
+			require("dap-python").test_runner = "pytest"
+		end,
+	},
 }
