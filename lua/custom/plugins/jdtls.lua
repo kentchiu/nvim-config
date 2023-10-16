a858101d50da51b41ae654cd0fe1c04282ba63f7
Kent Chiu <kent.cwg@gmail.com>
2023-10-17 01:15:40 +0800

    java

--- lua/custom/plugins/jdtls.lua
+++ lua/custom/plugins/jdtls.lua
@@ -5,6 +5,7 @@ return {
     config = function()
       -- local JDTLS_LOCATION = "/home/kent/.local/share/nvim/mason/bin/jdtls"
       local JDTLS_LOCATION = vim.fn.stdpath "data" .. "/mason/bin/jdtls"
+      print("JDTLS_LOCATION: " .. JDTLS_LOCATION)
       local config = {
         cmd = { JDTLS_LOCATION},
         root_dir = vim.fs.dirname(vim.fs.find({ 'gradlew', '.git', 'mvnw' }, { upward = true })[1]),
